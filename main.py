3afa68adadf7fa116481d035f331565ee37f21af
Max <podmax25@gmail.com>
2021-12-27 12:54:46 +0300

    Начало змейки

--- main.py
+++ main.py
@@ -2,23 +2,39 @@ import pygame
 import random
 
 COLORS = [pygame.Color('black'), pygame.Color('white'), pygame.Color('blue')]
+x = 0
+y = 0
+size = width, height = 600, 600
 
 
 class Snake:
     def __init__(self, screen, width, height):
+        global x, y
         x = width // 2
         y = height // 2
         pygame.draw.rect(screen, COLORS[2], [x, y, 10, 10])
 
-    def spawn_snake(self, screen, width, height):
-        x = width // 2
-        y = height // 2
-        pygame.draw.rect(screen, COLORS[2], [x, y, 10, 10])
+    def snake_move(self, screen, x1, y1):
+        global x, y
+        x = x1
+        y = y1
+        pygame.draw.rect(screen, COLORS[2], [x1, y1, 10, 10])
+
+
+def move(movement, screen):
+    global height, x, y, width
+    if movement == 'up' and x != height:
+        Snake.snake_move(screen, x + 3, y)
+    elif movement == 'down' and x > 0:
+        Snake.snake_move(screen, x - 3, y)
+    elif movement == 'right' and y != width:
+        Snake.snake_move(screen, x, y + 3)
+    elif movement == 'left' and y != 0:
+        Snake.snake_move(screen, x, y - 3)
 
 
 def main():
     pygame.init()
-    size = width, height = 600, 600
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Змейка')
     snake = Snake(screen, width, height)
@@ -27,8 +43,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    move("up", screen)
+                elif event.key == pygame.K_DOWN:
+                    move("down", screen)
+                elif event.key == pygame.K_LEFT:
+                    move("left", screen)
+                elif event.key == pygame.K_RIGHT:
+                    move("right", screen)
         screen.fill(COLORS[0])
-        snake.spawn_snake(screen, width, height)
         pygame.display.flip()
     pygame.quit()
 
