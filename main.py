6388e60ac3164dbdb05be0ca23944d8e109d0292
Max <podmax25@gmail.com>
2021-12-27 15:48:45 +0300

    Начало змейки

--- main.py
+++ main.py
@@ -1,50 +1,48 @@
 import pygame
 import random
 
+clock = pygame.time.Clock()
+fps = 30
 COLORS = [pygame.Color('black'), pygame.Color('white'), pygame.Color('blue')]
-x = 0
-y = 0
 size = width, height = 600, 600
+x0 = 0
+y0 = 0
 
 
 class Snake:
     def __init__(self, screen, width, height):
-        global x, y
         x = width // 2
         y = height // 2
         pygame.draw.rect(screen, COLORS[2], [x, y, 10, 10])
 
     def spawn_snake(self, screen, width, height):
-        global x, y
         x = width // 2
         y = height // 2
         pygame.draw.rect(screen, COLORS[2], [x, y, 10, 10])
+        return x, y
 
     def snake_move(self, screen, x1, y1):
-        global x, y
-        x = x1
-        y = y1
         pygame.draw.rect(screen, COLORS[2], [x1, y1, 10, 10])
 
 
-def move(movement, screen):
-    global height, x, y, width
+def move(movement, screen, x, y):
+    global height, width
     if movement == 'up' and x != height:
         Snake.snake_move(screen, screen, x + 3, y)
     elif movement == 'down' and x > 0:
-        Snake.snake_move(screen, screen, x - 3, y)
+        Snake.snake_move(screen, x - 3, y)
     elif movement == 'right' and y != width:
         Snake.snake_move(screen, screen, x, y + 3)
-    elif movement == 'left' and y != 0:
+    elif movement == 'left' and y > 0:
         Snake.snake_move(screen, screen, x, y - 3)
 
 
 def main():
+    global x0, y0
     pygame.init()
     screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Змейка')
     snake = Snake(screen, width, height)
-    snake.spawn_snake(screen, width, height)
     running = True
     while running:
         for event in pygame.event.get():
@@ -52,15 +50,21 @@ def main():
                 running = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
-                    move("up", screen)
+                    move("up", screen, x0, y0)
+                    x0 += 3
                 elif event.key == pygame.K_DOWN:
-                    move("down", screen)
+                    move("down", screen, x0, y0)
+                    x0 -= 3
                 elif event.key == pygame.K_LEFT:
-                    move("left", screen)
+                    move("left", screen, x0, y0)
+                    y0 += 3
                 elif event.key == pygame.K_RIGHT:
-                    move("right", screen)
+                    move("right", screen, x0, y0)
+                    y0 -= 3
         screen.fill(COLORS[0])
+        x0, y0 = snake.spawn_snake(screen, width, height)
         pygame.display.flip()
+        clock.tick(fps)
     pygame.quit()
 
 
