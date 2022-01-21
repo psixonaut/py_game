a0a254650194c5fa3e36966dda2039b1be05a589
VarvaraEzerskaya <94551739+VarvaraEzerskaya@users.noreply.github.com>
2022-01-21 23:58:50 +0300

    Add files via upload

--- space_invaders.py
+++ space_invaders.py
@@ -1,7 +1,5 @@
 import pygame
 
-score = 0
-
 
 class Alien:
     def __init__(self, game, x, y):
@@ -11,15 +9,15 @@ class Alien:
         self.size = 30
 
     def spawn(self, speed_up_coef):
-        pygame.draw.rect(self.game.screen, (Game.COLORS[2]), pygame.Rect(self.x, self.y, self.size, self.size))
+        pygame.draw.rect(self.game.screen, (pygame.Color((155, 93, 229))), pygame.Rect(self.x, self.y, self.size, self.size))
         self.y += speed_up_coef
 
     def check_for_collision(self, game):
         for laser in game.rockets:
             if (laser.x < self.x + self.size and
-                    laser.x > self.x - self.size and
-                    laser.y < self.y + self.size and
-                    laser.y > self.y - self.size):
+                laser.x > self.x - self.size and
+                laser.y < self.y + self.size and
+                laser.y > self.y - self.size):
                 game.rockets.remove(laser)
                 game.aliens.remove(self)
 
@@ -31,7 +29,7 @@ class Hero:
         self.y = y
 
     def spawn(self):
-        pygame.draw.rect(self.game.screen, (Game.COLORS[1]), pygame.Rect(self.x, self.y, 8, 5))
+        pygame.draw.rect(self.game.screen, (pygame.Color((0, 187, 249))), pygame.Rect(self.x, self.y, 8, 5))
 
 
 class Generator:
@@ -50,71 +48,74 @@ class Rocket:
         self.game = game
 
     def spawn(self):
-        pygame.draw.rect(self.game.screen, (Game.COLORS[3]), pygame.Rect(self.x, self.y, 2, 4))
+        pygame.draw.rect(self.game.screen, (pygame.Color((241, 91, 181))), pygame.Rect(self.x, self.y, 2, 4))
         self.y -= 2
 
 
 class Game:
-    screen = None
-    aliens = []
-    rockets = []
-    lost = False
-    COLORS = [pygame.Color((16, 20, 25)), pygame.Color((0, 187, 249)),
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.aliens = []
+        self.rockets = []
+        self.lost = False
+        self.COLORS = [pygame.Color((16, 20, 25)), pygame.Color((0, 187, 249)),
               pygame.Color((155, 93, 229)), pygame.Color(241, 91, 181)]
-
-    def start(self, width, height):
-        pygame.init()
-        self.width = height
-        self.height = width
         self.screen = pygame.display.set_mode((width, height))
-        pygame.display.set_caption('Космические захватчики')
-        self.clock = pygame.time.Clock()
-        running = True
-
-        hero = Hero(self, width / 2, height - 20)
-        generator = Generator(self)
-        rocket = None
-
-        while running:
-            if len(self.aliens) == 0:
-                self.write_text("ПОБЕДА")
-
-            pressed = pygame.key.get_pressed()
-            if pressed[pygame.K_LEFT]:
-                hero.x -= 2 if hero.x > 20 else 0
-            elif pressed[pygame.K_RIGHT]:
-                hero.x += 2 if hero.x < width - 20 else 0
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not self.lost:
-                    self.rockets.append(Rocket(self, hero.x, hero.y))
-
-            pygame.display.flip()
-            self.clock.tick(60)
-            self.screen.fill(Game.COLORS[0])
-
-            for inwader in self.aliens:
-                inwader.spawn(0.4)
-                inwader.check_for_collision(self)
-                if (inwader.y > height):
-                    self.lost = True
-                    self.write_text("ПОРАЖЕНИЕ")
-
-            for laser in self.rockets:
-                laser.spawn()
-
-            if not self.lost:
-                hero.spawn()
-        pygame.quit()
 
     def write_text(self, text):
         pygame.font.init()
         font = pygame.font.SysFont('Monaco', 50)
-        textsurface = font.render(text, True, (Game.COLORS[1]))
+        textsurface = font.render(text, True, (self.COLORS[1]))
         self.screen.blit(textsurface, (320, 160))
 
 
-game = Game()
-game.start(800, 800)
+def start():
+    game = Game(800, 800)
+    pygame.init()
+    pygame.mixer.music.load('space_invaders.mp3')
+    pygame.mixer.music.play()
+
+    pygame.display.set_caption('Космические захватчики')
+    clock = pygame.time.Clock()
+    running = True
+
+    hero = Hero(game, game.width / 2, game.height - 20)
+    generator = Generator(game)
+    rocket = None
+
+    while running:
+        if len(game.aliens) == 0:
+            game.write_text("ПОБЕДА")
+
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_LEFT]:
+            hero.x -= 2 if hero.x > 20 else 0
+        elif pressed[pygame.K_RIGHT]:
+            hero.x += 2 if hero.x < game.width - 20 else 0
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and not game.lost:
+                game.rockets.append(Rocket(game, hero.x, hero.y))
+
+        pygame.display.flip()
+        clock.tick(60)
+        game.screen.fill(game.COLORS[0])
+
+        for inwader in game.aliens:
+            inwader.spawn(0.4)
+            inwader.check_for_collision(game)
+            if (inwader.y > game.height):
+                game.lost = True
+                game.write_text("ПОРАЖЕНИЕ")
+
+        for laser in game.rockets:
+            laser.spawn()
+
+        if not game.lost:
+            hero.spawn()
+    pygame.quit()
+
+
